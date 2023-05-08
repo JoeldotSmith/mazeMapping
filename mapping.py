bb401cd594ba10f49a74df4bb0210a310b1916d4
Joel Smith <joelsmith2004@gmail.com>
2023-05-08 07:50:02 +0000

    8 May 2023, 15:50

--- mapping.py
+++ mapping.py
@@ -16,10 +16,10 @@ def drawLine(x, y, lidar_x, lidar_y):
     if lidar_y < 0:
         lidar_y = 0
 
-    LCDLine(2*int(128*x/SIM_WORLD_SIZE), 256-2*int(y/SIM_WORLD_SIZE), 2 *
-            int(128*lidar_x/SIM_WORLD_SIZE), 256-2*int(128*lidar_y/SIM_WORLD_SIZE), WHITE)
-    LCDCircle(2*int(128*lidar_x/SIM_WORLD_SIZE), 256-2 *
-              int(128*lidar_y/SIM_WORLD_SIZE), 5, GREEN, 1)
+    LCDLine(2*int(128*y/SIM_WORLD_SIZE), 256-2*int(x/SIM_WORLD_SIZE), 2 *
+            int(128*lidar_y/SIM_WORLD_SIZE), 256-2*int(128*lidar_x/SIM_WORLD_SIZE), WHITE)
+    LCDCircle(2*int(128*lidar_y/SIM_WORLD_SIZE), 256-2 *
+              int(128*lidar_x/SIM_WORLD_SIZE), 5, GREEN, 1)
 
 
 def mapping():
